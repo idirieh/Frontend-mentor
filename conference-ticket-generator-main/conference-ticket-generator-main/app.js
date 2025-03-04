ce38e93c96231396032459d26b2422fe53521328
Ismail <ismael.dirieh@gmail.com>
2025-03-04 14:53:12 +0300

    update Js file in folder number20

--- conference-ticket-generator-main/conference-ticket-generator-main/app.js
+++ conference-ticket-generator-main/conference-ticket-generator-main/app.js
@@ -2,10 +2,28 @@ const fullNmae = document.getElementById('full-name');
 const userEmail = document.getElementById('email');
 const userName = document.getElementById('user-name');
 const submitBtn = document.getElementById('submit-btn');
+const containers = document.querySelectorAll('.container')
+// Ticket Side
+const showUserFullName = document.getElementById('user_fullname');
+const showUserEmail = document.getElementById('user-email');
+const showName = document.querySelector('.person-name')
+const showUserId = document.querySelector('.person-username')
+
 
 submitBtn.addEventListener('click', () => {
-    console.log(fullNmae.value)
-    console.log(userEmail.value)
-    console.log(userName.value)
+    if (containers.length >= 2) {
+        containers[0].classList.toggle("hidden"); // Hide first container
+        containers[1].classList.toggle("hidden"); // Show second container
+    }
+    showUserFullName.innerText = fullNmae.value;
+    showUserEmail.innerText = userEmail.value;
+    showName.innerText = fullNmae.value;
+    showUserId.innerText = userName.value;
+
+    fullNmae.value = " "
+    userEmail.value = " "
+    userName.value = " "
+
 })
   
+
