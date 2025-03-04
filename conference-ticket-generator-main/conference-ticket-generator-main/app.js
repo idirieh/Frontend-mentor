2a07aa1c20d0578c6374c1f8f71d5c0bb5e35d4a
Ismail <ismael.dirieh@gmail.com>
2025-03-04 13:38:50 +0300

    update remove insdie folder

--- conference-ticket-generator-main/conference-ticket-generator-main/app.js
+++ conference-ticket-generator-main/conference-ticket-generator-main/app.js
@@ -1,2 +1,11 @@
+const fullNmae = document.getElementById('full-name');
+const userEmail = document.getElementById('email');
+const userName = document.getElementById('user-name');
+const submitBtn = document.getElementById('submit-btn');
 
+submitBtn.addEventListener('click', () => {
+    console.log(fullNmae.value)
+    console.log(userEmail.value)
+    console.log(userName.value)
+})
   
